f305d8e6d45e86ceb061e829023b9d87881f0b41
amnabilr <andinabil2001@gmail.com>
2022-07-11 18:31:40 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>@yield('title')</title>
+    @stack('prepend-style')
+    @include('includes.style')
+    <link
+      href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap"
+      rel="stylesheet"
+    />
+    @stack('addon-style')
+  </head>
+  <body>
+    
+    @include('includes.navbar')
+    
+    @yield('content')
+
+    @include('includes.footer')
+
+    @stack('prepend-script')
+
+    @include('includes.script')
+
+    @stack('addon-script')
+
+  </body>
+</html>
